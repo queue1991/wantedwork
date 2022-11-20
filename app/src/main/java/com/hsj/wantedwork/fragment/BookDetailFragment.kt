4876661e7d4864da62381913e9fe49097a233b50
queue1991@naver.com <queue1991@naver.com>
2022-11-21 00:05:17 +0900

    BookDetailFragment loading View 추가

--- app/src/main/java/com/hsj/wantedwork/fragment/BookDetailFragment.kt
+++ app/src/main/java/com/hsj/wantedwork/fragment/BookDetailFragment.kt
@@ -1,7 +1,12 @@
 package com.hsj.wantedwork.fragment
 
+import android.graphics.Bitmap
 import android.os.Bundle
 import android.view.View
+import android.view.View.GONE
+import android.view.View.VISIBLE
+import android.webkit.WebChromeClient
+import android.webkit.WebView
 import android.webkit.WebViewClient
 import com.hsj.wantedwork.R
 import com.hsj.wantedwork.base.view.BaseKotlinFragment
@@ -18,11 +23,29 @@ class BookDetailFragment : BaseKotlinFragment<DetailViewModel>(){
         get() = R.id.container
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        webview.webViewClient = WebViewClient()
         webview.settings.loadWithOverviewMode = true
         webview.settings.setSupportZoom(true)
         webview.settings.builtInZoomControls = true
         webview.settings.useWideViewPort = true
+
+        webview.webChromeClient = object : WebChromeClient() {
+            override fun onProgressChanged(view: WebView?, newProgress: Int) {
+                progress_horizontal.progress = newProgress
+                super.onProgressChanged(view, newProgress)
+            }
+        }
+
+        webview.webViewClient = object : WebViewClient() {
+            override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
+                progress_horizontal.visibility = VISIBLE
+                super.onPageStarted(view, url, favicon)
+            }
+            override fun onPageFinished(view: WebView?, url: String?) {
+                progress_horizontal.visibility = GONE
+                super.onPageFinished(view, url)
+            }
+        }
+
         webview.loadUrl(viewModel.getPreviewLink())
         super.onViewCreated(view, savedInstanceState)
     }
